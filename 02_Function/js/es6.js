549567b428d1fb386e2308e4501a08d29d846457
Artem Shanin <mefistoggc@mail.ru>
2016-07-10 20:21:25 +0300

    remarks 02

--- 02_Function/js/es6.js
+++ 02_Function/js/es6.js
@@ -1,8 +1,7 @@
 // consoleRec(array[, first index]);
 function consoleRec(arr, num) {
 	var lengthRec = arr.length - 1;
-	var arr = arr;
-	var length = num || 0;
+	var length = (typeof num === 'number') ? num : 0;
 
 	console.log(arr[length]);
 	return length !== lengthRec ? consoleRec(arr, length + 1) : false;
