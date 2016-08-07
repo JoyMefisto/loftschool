22368b92e8980055eb7e45fa8ab5f0ac3b94ca32
Artem Shanin <mefistoggc@mail.ru>
2016-08-07 23:18:09 +0300

    добавлен вывод друзей пользователя, и сортировка по дню рождения

--- 09_SDK/two/js/app.js
+++ 09_SDK/two/js/app.js
@@ -24,9 +24,10 @@ new Promise(function(resolve){
         }, 2);
     });
 }).then(function(){
+    // Возвращаем Имя Фамилию пользователя (просклонённую)
     return new Promise(function(resolve, reject) {
         VK.api('users.get', { 'fields': 'photo_200' ,'name_case': 'gen'}, function (response) {
-            console.log(response);
+            // console.log(response);
             if (response.error) {
                 reject(new Error(response.error.error_msg));
             } else {
@@ -38,13 +39,50 @@ new Promise(function(resolve){
     });
 }).then(function(){
     return new Promise(function(resolve, reject) {
-        VK.api('friends.get', { 'fields': 'photo_200' ,'name_case': 'nom'}, function (response) {
-            console.log(response);
+        VK.api('friends.get', { v: '5.53', 'fields': 'photo_100, bdate'}, function (response) {
+            // console.log(response);
             if (response.error) {
                 reject(new Error(response.error.error_msg));
             } else {
-                headerInfo.textContent = 'Музыка на странице '
-                    + response.response[0].first_name + ' ' + response.response[0].last_name;
+                let obMyFriends = [];
+                let nowDate = new Date();
+                let myDate = {
+                            day : nowDate.getDate(),
+                            month : nowDate.getMonth() + 1
+                            };
+                for(let item = 0; item < response.response.items.length; item++){
+                    obMyFriends[obMyFriends.length] = { first_name : response.response.items[item].first_name,
+                                                        last_name : response.response.items[item].last_name,
+                                                        photo_100 : response.response.items[item].photo_100,
+                                                        bdate : response.response.items[item].bdate
+                                                        }
+                }
+
+                let newObMyFriends = obMyFriends.filter(function(elem){
+                    if(elem.bdate !== undefined){
+                        return elem;
+                    }
+                }).sort(function(a, b) {
+                    let aDateArr = a.bdate.split('.');
+                    let bDateArr = b.bdate.split('.');
+
+
+                    if(parseInt(aDateArr[1]) > parseInt(bDateArr[1])) return 1;
+                    else if(parseInt(aDateArr[1]) < parseInt(bDateArr[1])) return -1;
+
+                    if(parseInt(aDateArr[0]) >= parseInt(bDateArr[0])) return 1;
+                    else if(parseInt(aDateArr[0]) < parseInt(bDateArr[0])) return -1;
+
+                });
+
+                console.log(myDate);
+
+
+                let source = template_el.innerHTML;
+                let templateFn = Handlebars.compile(source);
+                let template = templateFn({ elems: newObMyFriends});
+                friends.innerHTML = template;
+
                 resolve();
             }
         });
